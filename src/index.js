4ca3a812f67e699c0ec23cc13635764591aabf48
j721 <juliadvu@gmail.com>
2020-04-14 14:43:47 -0500

    Creating redux store and connecting it to React app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import {carReducer} from './reducers/carReducer';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+
+//Step 1- create redux store and connect it to our React app
+const store = createStore(carReducer);
+console.log('current state:', store.getState());
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+        <Provider>
+            <App />
+        </Provider>,
+    rootElement
+);
